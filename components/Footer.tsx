3ccdbad701d3191514b12c648e15070df05545c6
JannatunNaeem141 <jannatunnaeem141@gmail.com>
2024-10-30 16:48:16 +0600

    color palette set at global.css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+
 export default function Footer() {
-  return <footer className="w-full h-12 bg-blue-200 mt-6"></footer>;
+  return (
+    <footer className="bg-card w-full mt-6 py-4 px-6 rounded-md border-default">
+      <div className="block md:flex md:justify-between text-muted">
+        <p className="sm:mb-0 text-xs md:text-sm">COPYRIGHT © 2024 <Link href='#'>Optimus Technologies</Link> All rights Reserved</p>
+        <p className="mb-0 text-xs md:text-sm">
+          Hand-crafted &amp; Made by{' '}
+          <a className="text-primary" target="__blank" href="https://codeshaper.net">
+            Codeshaper
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  darkMode: ['class'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        // primary: '#2563eb',
+        // secondary: {
+        //   '600': '#475569',
+        //   '600-dark': '#94a3b8',
+        //   '700': '#334155',
+        //   '700-dark': '#cbd5e1',
+        //   '900': '#0f172a',
+        //   '900-dark': '#f1f5f9',
+        // },
+        // muted: '#64748b',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 };
 export default config;
