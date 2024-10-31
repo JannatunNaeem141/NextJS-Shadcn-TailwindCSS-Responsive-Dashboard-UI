294b2ffcab7e7057754e76e2e02a618cbaab14d8
JannatunNaeem141 <jannatunnaeem141@gmail.com>
2024-10-31 16:27:20 +0600

    sidebar logo section and sheet sidebar update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,19 +12,26 @@ export default function Sidebar({ isSidebarCollapsed, isSheetOpen, setIsSheetOpe
       {/* Sidebar at XL device */}
       <aside className={`xl:block hidden fixed top-0 bottom-0 sm:p-6 p-3`}>
         <div className={`${isSidebarCollapsed ? 'xl:w-[72px]' : 'xl:w-[248px]'} h-full border-r border-default rounded-md overflow-hidden flex flex-col bg-card transition-width duration-300`}>
-          <div className="p-4 border-b border-default">Logo</div>
-          <div className="h-full flex-1 overflow-y-auto">fgsf</div>
+          {/* Logo */}
+          <div className="p-4 border-b border-default">
+            <div className={`${isSidebarCollapsed ? 'block' : 'hidden'}`}>Fav</div>
+            <div className={`${isSidebarCollapsed ? 'hidden' : 'block'}`}>Logo</div>
+          </div>
+          {/* Nav Links */}
+          <div className="h-full flex-1 overflow-y-auto sm:p-4 p-3">
+            <p className="h-44 bg-gray-700">a</p>
+          </div>
         </div>
       </aside>
-      
+
       {/* Sidebar at LG and Small device */}
       <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
-        <SheetContent side="left">
+        <SheetContent side="left" className="border-default">
           <SheetHeader className="hidden">
             <SheetTitle></SheetTitle>
             <SheetDescription></SheetDescription>
           </SheetHeader>
-          <div className="p-4 border-b border-default">Logo</div>
+          <div className="py-4 border-b border-default">Dashboard</div>
           <div className="h-full flex-1 overflow-y-auto">Sidebar Content</div>
         </SheetContent>
       </Sheet>
